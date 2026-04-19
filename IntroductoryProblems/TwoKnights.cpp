326f081189ff0158a71fb7486484713e72d8bdb3
LokeshRaju1234 <loki65628@gmail.com>
2026-04-20 00:24:13 +0530

    Solved TwoKnights Problem (Combination Problem)

---
+++ IntroductoryProblems/TwoKnights.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(string s)
+{
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        // k == 1 2 3 .. n boards for each board we need to find the
+        // how to place two knights so that they cannot attack
+        long long k = i * i;
+        // to know combinations to place two knights n(n - 1) / 2;
+        // knight moves only 2 steps in one direction and 1 step sidewards
+        // there are 2 rectangles only attacks will happen i.e in 2 x 3 and 3 x 2
+        // formula is 2 * (k - 2) (k - 1) and each rectangle gives 2 attacks
+        //-->so to remove attacks 4 * (k - 2)(k - 1)
+        long long ans = k * (k - 1) / 2 - 4 * (i - 1) * (i - 2);
+        cout << ans << endl;
+    }
+
+    return 0;
+}
